b5fc169c517d61afd2afe9c9657adc06e7912ca9
Iranildo Santos Batalha (UEA) <isb.eng18@dmz.org.br>
2022-10-13 12:42:17 -0400

    adding workspace config

---
+++ conf.sh
@@ -0,0 +1,15 @@
+export PER_ROOT=~/pessoal
+
+# -- DEV CONFIG
+export WS="$PER_ROOT/dev"
+
+
+# -- LIC CONFIG
+export LIC="$WS/lic"
+
+# -- -- REPOS
+export LICF="$LIC/srbr-m-lic-frontend"
+export LICA="$LIC/srbr-m-lic-app"
+export LICB="$LIC/srbr-m-lic-backend"
+
+
